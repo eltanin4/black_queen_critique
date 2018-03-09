95d621261105f6202455c8c1809103c2af4d61dd
Akshit Goyal <goyalsharp@gmail.com>
2018-03-09 18:19:09 +0530

    Understood that gainLoss overestimates events, does not give bonafide gains/losses

---
+++ setup_analysis.py
@@ -0,0 +1,71 @@
+import numpy as np
+import pickle
+import pandas as pd
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+from uniqify import uniqify
+from unlistify import unlistify
+NUM_ORGS = 301
+
+# Getting the names of the abbrevations from KEGG for KOMODO comparison.
+orgNameDict = {}
+with open('prok_kegg_abbr_with_names_dups_removed.txt', 'r') as f:
+    for thisOrg in range(NUM_ORGS):              
+        thisLine = f.readline()    
+        orgNameDict[ thisLine[:3] ] = thisLine[4:-1]
+
+prokNames = list( orgNameDict.keys() ) 
+
+# Getting sizes for each organism.
+rxnDict = {}
+for thisOrg in prokNames:
+    rxnDict[ thisOrg ] = list( np.genfromtxt( 'organism_reactions/' + thisOrg + '.txt' ) )
+
+# # March 2, 2018
+# # Getting KOMODO growers again.
+# import pandas as pd
+org_to_media_df = pd.read_html('komodo_org_to_media.html')[1]
+
+# March 5, 2018
+for thisInd, thisOrg in enumerate( org_to_media_df[2][1:] ):
+    try:
+        org_to_media_df[2][thisInd] = ' '.join( thisOrg.split( )[ : 2 ] )
+    except:
+        pass
+
+# Computing whether a medium is listed for a species or not.
+KOMODOisThereDict = {}
+for thisOrg in prokNames:
+    KOMODOisThereDict[ thisOrg ] = bool( org_to_media_df[2].str.contains( 
+                                   orgNameDict[ thisOrg ] ).any() )
+
+# Getting the normalized and manually checked ProTraits growers.
+invOrgNameDict = { value: key for key, value in orgNameDict.items() }
+FULLPROTRAITSisThereDict = {}
+proCultDF = pd.read_csv('filtered_ProTraits_binaryIntegratedPr0.95.csv')
+for idx, thisRow in proCultDF.iterrows():
+    try:
+        thisOrgName = invOrgNameDict[ ' '.join( thisRow[1].split( )[ : 2 ] ) ]
+    except:
+        continue
+
+    if bool( thisRow[-1] == True ):
+        FULLPROTRAITSisThereDict[ thisOrgName ] = 1
+    else:
+        try:
+            FULLPROTRAITSisThereDict[ thisOrgName ]
+        except:
+            FULLPROTRAITSisThereDict[ thisOrgName ] = 0
+
+# Getting sizes for each organism.
+rxnDict = {}
+for thisOrg in FULLPROTRAITSisThereDict.keys():
+    rxnDict[ thisOrg ] = list( np.genfromtxt( 'organism_reactions/' + thisOrg + '.txt' ) )
+
+# Comparing sizes.
+sizes_ind = [ len( rxnDict[ thisOrgName ] ) 
+              for thisOrgName in rxnDict.keys() 
+              if FULLPROTRAITSisThereDict[ thisOrgName ] ] 
+sizes_dep = [ len( rxnDict[ thisOrgName ] ) 
+              for thisOrgName in rxnDict.keys() 
+              if not FULLPROTRAITSisThereDict[ thisOrgName ] ] 
--- setup_gloome_njs16.py
+++ setup_gloome_njs16.py
@@ -1,7 +1,13 @@
+import numpy as np
 import pickle
+import pandas as pd
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+from uniqify import uniqify
+from unlistify import unlistify
 
 njs16_rxnDict = pickle.load( open( 'dict_njs16_rxn.dat', 'rb' ) )
-njs16_ind_Dict = pickle.load( open( 'dict_njs16_ind.dat', 'rb' ) )
+njs16_ind_dict = pickle.load( open( 'dict_njs16_ind.dat', 'rb' ) )
 reaction_ids = sorted( uniqify( unlistify( list( njs16_rxnDict.values() ) ) ) )
 njs16_pa_dict = {}
 for thisOrg in njs16_ind_dict.keys():
@@ -24,3 +30,14 @@ with open( 'njs16_msa_file.txt', 'w' ) as msaFile:
 running_string = ''
 for tn in njs16_pa_dict:
     running_string += ' \"' + inv_img_to_name_dict[ '_'.join( tn.split() ) ] + '_' + '_'.join( tn.split() ) + '\",'
+
+# Getting the pairwise matrices for phylogenetic distances, gene losses and gains.
+pdMat = pd.read_csv( 'njs16_gainLoss_results/njs16_anctree_pairwise.csv' )
+gsMat = pd.read_csv( 'njs16_gainLoss_results/njs16_gains_pairwise.csv' )
+lsMat = pd.read_csv( 'njs16_gainLoss_results/njs16_loss_pairwise.csv' )
+org_names = pdMat.loc[:, pdMat.columns[0]].values   # Here, the names are in the right order.
+
+isIndDict = {}
+for tn in njs16_ind_dict:
+    orgName = inv_img_to_name_dict[ '_'.join( tn.split() ) ] + '_' + '_'.join( tn.split() )
+    isIndDict[ orgName ] = njs16_ind_dict[ tn ]
